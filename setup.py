4e5cebaa481bcf9da05fccb8ac81ef3c3c6c4f78
Zeming Lin <zeming@evolutionaryscale.ai>
2024-10-08 22:17:28 +0000

    chain package name for pixi

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ extra_deps['testing'] = [
 
 extra_deps['all'] = sorted({dep for deps in extra_deps.values() for dep in deps})
 
-package_name = os.environ.get('MOSAIC_PACKAGE_NAME', 'mosaicml-streaming')
+package_name = os.environ.get('MOSAIC_PACKAGE_NAME', 'streaming')
 
 if package_name != 'mosaicml-streaming':
     print(f'Building mosaicml-streaming as {package_name}')
